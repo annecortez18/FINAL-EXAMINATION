67a5fcaa362249308f9e2f3e4d679f36324ef6e2
Student 1 <cortezroxanne700@gmail.com>
2024-06-24 15:46:06 +0800

    first commit

--- employee.py
+++ employee.py
@@ -9,3 +9,14 @@ class Employee:
         result = Connector.cursor.fetchall()
 
         return result
+
+        def add_employee(emp_id, lname, fname, mname):
+            query = "INSERT INTO employees VALUES (%s, %s, %s, %s,)"
+
+            try:
+                Connector.cursor.execute(querey,(emp_id, lname, fname, mname))
+                Connector.db.commit()
+
+                return True
+            except:
+                return False
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template, request,redirect
+from flask import Flask, render_template, request, redirect, session
 from users import Users
 from employee import Employee
+
 app = Flask(__name__)
+app.secret_key = "ANNE"
 
 @app.route('/')
 def index():
@@ -12,7 +14,7 @@ def check_user():
     username = request.form["username"]
     password = request.form["password"]
 
-    result = Users.check_user(username,password)
+    result = Users.check_user(username, password)
 
     if result:
         return redirect('/employee-list')
@@ -21,8 +23,60 @@ def check_user():
 
 @app.route('/employee-list')
 def employee_list():
-    employees = Employee.get_all()
-    return render_template('employee_list.html', employees=employees)
+    employee = Employee.get_all()
+    
+    if "message" not in session:
+        session["message"] = ""
+
+    return render_template('employee_list.html', employee=employee, message=session.get('message'))
+
+@app.route('/add-form')
+def add_form():
+    return render_template('add_employee.html')
+
+@app.route('/add-employee', methods=["POST"])
+def add_employee():
+    emp_id = request.form["emp_id"]
+    lname = request.form["lname"]
+    fname = request.form["fname"]
+    mname = request.form["mname"]
+    
+    success = Employee.add_employee(emp_id, lname, fname, mname)
+
+    if success:
+        session["message"] = "Successfully added"
+    else:
+        session["message"] = "Failed to add employee"
+
+    return redirect('/employee-list')
+
+@app.route('/update-form/<emp_id>')
+def update_form(emp_id):
+    employee = Employee.get_employee(emp_id)
+    if employee:
+        return render_template('update_employee.html', employee=employee)
+    else:
+        session["message"] = "Employee not found."
+        return redirect('/employee-list')
+
+@app.route('/update-employee', methods=["POST"])
+def update_employee():
+    emp_id = request.form["emp_id"]
+    lname = request.form["lname"]
+    fname = request.form["fname"]
+    mname = request.form["mname"]
+    
+    success = Employees.update_employee(emp_id, lname, fname, mname)
+
+    if success:
+        session["message"] = "Successfully updated"
+    else:
+        session["message"] = "Failed to update employee"
+
+    return redirect('/employee-list')
+
 
 if __name__ == '__main__':
     app.run()
+
+
